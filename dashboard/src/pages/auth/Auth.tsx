e402db06a79db2f21237817a0d1bebe0911363a2
Alexey Vorobyev <alexvorobev01@gmail.com>
2022-08-30 00:04:35 +0700

    button and input added

--- dashboard/src/pages/auth/Auth.tsx
+++ dashboard/src/pages/auth/Auth.tsx
@@ -1,7 +1,14 @@
+import { Input, Button } from "components/core";
+
 const Auth = () => {
   return (
     <div>
       <h1>Auth</h1>
+      <div>
+        <Input placeholder="email" />
+        <Input placeholder="password" type='password' />
+        <Button>Send</Button>
+      </div>
     </div>
   );
 };
