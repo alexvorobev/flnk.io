51b8ac4dc2aad84d3961cdd6f51fac7097e62fc7
Alexey Vorobyev <alexvorobev01@gmail.com>
2022-08-29 22:02:21 +0700

    linter and prettier added

--- dashboard/src/pages/App.tsx
+++ dashboard/src/pages/App.tsx
@@ -1,9 +1,12 @@
+import Auth from 'pages/auth';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       FLNK Dashboard
+      <Auth />
     </div>
   );
 }
---
+++ dashboard/src/pages/auth/Auth.tsx
@@ -0,0 +1,6 @@
+const Auth = () => {
+    return (<div><h1>Auth</h1></div>
+    );
+}
+
+export default Auth;
