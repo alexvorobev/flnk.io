1bd6441a758f5784e43ee0e69800fefab8c0b0d3
Alex Vorobev <alexvorobev01@gmail.com>
2022-11-20 14:24:41 +0700

    formatted

--- dashboard/src/components/NavigationTabs/NavigationTabs.tsx
+++ dashboard/src/components/NavigationTabs/NavigationTabs.tsx
@@ -4,18 +4,27 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { PROTECTED_ROUTES } from 'routes';
 
 const menuLinks = [
-    { label: 'Links', to: PROTECTED_ROUTES.HOME },
-    { label: 'Users', to: PROTECTED_ROUTES.USERS },
-]
+  { label: 'Links', to: PROTECTED_ROUTES.HOME },
+  { label: 'Users', to: PROTECTED_ROUTES.USERS },
+];
 
 export const NavigationTabs = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <TabNavigation marginBottom={32}>
       {menuLinks.map((tab, index) => (
-        <Tab key={tab.label} is='div' size="large" height={48} paddingX={24} onSelect={() => navigate(tab.to)} id={tab.label} isSelected={location.pathname === tab.to}>
+        <Tab
+          key={tab.label}
+          is='div'
+          size='large'
+          height={48}
+          paddingX={24}
+          onSelect={() => navigate(tab.to)}
+          id={tab.label}
+          isSelected={location.pathname === tab.to}
+        >
           {tab.label}
         </Tab>
       ))}
